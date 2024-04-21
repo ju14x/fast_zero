4156817868ad94e407978c3213c6b434a7c13271
Juliana Machado <jfcaiado@hotmail.com>
2024-04-21 00:37:24 -0300

    fix: test_root

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,14 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_root_should_return_200_hello():
+def test_root_should_return_ok_and_hello():
     client = TestClient(app)
 
     response = client.get('/')
 
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá!'}
