7bd2c1cf55783e20b37c883cfb8de440f26e395e
Juliana Machado <jfcaiado@hotmail.com>
2024-07-09 15:06:04 -0300

    feat: create user endpoint with temporary in-memory database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,14 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_should_return_ok_and_hello():
-    client = TestClient(app)
-
+def test_root_should_return_ok_and_hello(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá!'}
 
 
-def test_root_should_return_html():
-    client = TestClient(app)
-
+def test_root_should_return_html(client):
     response = client.get('/html')
 
     assert response.status_code == HTTPStatus.OK
@@ -32,3 +24,21 @@ def test_root_should_return_html():
       </body>
     </html>"""
     )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
