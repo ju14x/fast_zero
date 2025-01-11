7ae17ab6fa59ef0e99054d58e0dc658059fb22d9
Juliana Machado <jfcaiado@hotmail.com>
2025-01-11 20:24:07 -0300

    feat: read_user route 200 and 404 tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -58,6 +58,24 @@ def test_read_users(client):
     }
 
 
+def test_read_user_return_ok(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_user_should_return_404(client):
+    response = client.get('/users/400')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_update_user(client):
     response = client.put(
         '/users/1',
